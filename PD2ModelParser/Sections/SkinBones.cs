7fb4420fed829f30fad5f9cbb71b5347dab9f102
Campbell Suter <znix@znix.xyz>
2018-02-04 16:12:50 +1300

    Move SkinBone's matrix serialization/deserialization to MathUtils

--- PD2ModelParser/Sections/SkinBones.cs
+++ PD2ModelParser/Sections/SkinBones.cs
@@ -36,9 +36,9 @@ namespace PD2ModelParser.Sections
                 this.objects.Add(instream.ReadUInt32());
             for (int x = 0; x < this.count; x++)
             {
-                this.rotations.Add(ReadMatrix(instream));
+                this.rotations.Add(MathUtil.ReadMatrix(instream));
             }
-            this.unknown_matrix = ReadMatrix(instream);
+            this.unknown_matrix = MathUtil.ReadMatrix(instream);
 
             this.remaining_data = null;
             if ((section.offset + 12 + section.size) > instream.BaseStream.Position)
@@ -71,9 +71,9 @@ namespace PD2ModelParser.Sections
                 outstream.Write(item);
             foreach (Matrix3D matrix in this.rotations)
             {
-                WriteMatrix(outstream, matrix);
+                MathUtil.WriteMatrix(outstream, matrix);
             }
-            WriteMatrix(outstream, unknown_matrix);
+            MathUtil.WriteMatrix(outstream, unknown_matrix);
 
             if (this.remaining_data != null)
                 outstream.Write(this.remaining_data);
@@ -96,49 +96,6 @@ namespace PD2ModelParser.Sections
             }
 
             return "[SkinBones] ID: " + this.id + " size: " + this.size + " bones: [ " + this.bones + " ] object3D_section_id: " + this.object3D_section_id + " count: " + this.count + " objects count: " + this.objects.Count + " objects:[ " + objects_string + " ] rotations count: " + this.rotations.Count + " rotations:[ " + rotations_string + " ] unknown_matrix: " + this.unknown_matrix + (this.remaining_data != null ? " REMAINING DATA! " + this.remaining_data.Length + " bytes" : "");
-        }
-
-        private static Matrix3D ReadMatrix(BinaryReader instream)
-        {
-            Matrix3D m;
-            m.M11 = instream.ReadSingle();
-            m.M12 = instream.ReadSingle();
-            m.M13 = instream.ReadSingle();
-            m.M14 = instream.ReadSingle();
-            m.M21 = instream.ReadSingle();
-            m.M22 = instream.ReadSingle();
-            m.M23 = instream.ReadSingle();
-            m.M24 = instream.ReadSingle();
-            m.M31 = instream.ReadSingle();
-            m.M32 = instream.ReadSingle();
-            m.M33 = instream.ReadSingle();
-            m.M34 = instream.ReadSingle();
-            m.M41 = instream.ReadSingle();
-            m.M42 = instream.ReadSingle();
-            m.M43 = instream.ReadSingle();
-            m.M44 = instream.ReadSingle();
-
-            return m;
-        }
-
-        private static void WriteMatrix(BinaryWriter outstream, Matrix3D matrix)
-        {
-            outstream.Write(matrix.M11);
-            outstream.Write(matrix.M12);
-            outstream.Write(matrix.M13);
-            outstream.Write(matrix.M14);
-            outstream.Write(matrix.M21);
-            outstream.Write(matrix.M22);
-            outstream.Write(matrix.M23);
-            outstream.Write(matrix.M24);
-            outstream.Write(matrix.M31);
-            outstream.Write(matrix.M32);
-            outstream.Write(matrix.M33);
-            outstream.Write(matrix.M34);
-            outstream.Write(matrix.M41);
-            outstream.Write(matrix.M42);
-            outstream.Write(matrix.M43);
-            outstream.Write(matrix.M44);
         }
     }
 }
