9bab72de08d3e394b78517d3023d354e7ac3fe18
Campbell Suter <znix@znix.xyz>
2019-06-03 15:40:02 +1200

    Remove exception handler from DieselExporter, and let the exceptions be handled by the caller

--- PD2ModelParser/Exporters/DieselExporter.cs
+++ PD2ModelParser/Exporters/DieselExporter.cs
@@ -54,123 +54,116 @@ namespace PD2ModelParser
             }
 
             //after each section, you go back and enter it's new size
-            try
+            using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
             {
-                using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+                using (BinaryWriter bw = new BinaryWriter(fs))
                 {
-                    using (BinaryWriter bw = new BinaryWriter(fs))
+
+                    bw.Write(-1); //the - (yyyy)
+                    bw.Write((UInt32)100); //Filesize (GO BACK AT END AND CHANGE!!!)
+                    int sectionCount = data.sections.Count;
+                    bw.Write(sectionCount); //Sections count
+
+                    foreach (Animation anim_sec in animation_sections)
                     {
-
-                        bw.Write(-1); //the - (yyyy)
-                        bw.Write((UInt32)100); //Filesize (GO BACK AT END AND CHANGE!!!)
-                        int sectionCount = data.sections.Count;
-                        bw.Write(sectionCount); //Sections count
-
-                        foreach (Animation anim_sec in animation_sections)
-                        {
-                            anim_sec.StreamWrite(bw);
-                        }
-
-                        foreach (Author author_sec in author_sections)
-                        {
-                            author_sec.StreamWrite(bw);
-                        }
-
-                        foreach (Material_Group mat_group_sec in material_group_sections)
-                        {
-                            mat_group_sec.StreamWrite(bw);
-                            foreach (uint id in mat_group_sec.items)
-                            {
-                                if (data.parsed_sections.Keys.Contains(id))
-                                    (data.parsed_sections[id] as Material).StreamWrite(bw);
-                            }
-                        }
-
-                        foreach (Object3D obj3d_sec in object3D_sections)
-                        {
-                            obj3d_sec.StreamWrite(bw);
-                        }
-
-                        foreach (Model model_sec in model_sections)
-                        {
-                            model_sec.StreamWrite(bw);
-                        }
-
-
-                        foreach (SectionHeader sectionheader in data.sections)
-                        {
-                            if (!data.parsed_sections.Keys.Contains(sectionheader.id))
-                                continue;
-                            object section = data.parsed_sections[sectionheader.id];
-
-                            if (section is Unknown)
-                            {
-                                (section as Unknown).StreamWrite(bw);
-                            }
-                            else if (section is Animation ||
-                                    section is Author ||
-                                    section is Material_Group ||
-                                    section is Material ||
-                                    section is Object3D ||
-                                    section is Model
-                                )
-                            {
-                                continue;
-                            }
-                            else if (section is Geometry)
-                            {
-                                (section as Geometry).StreamWrite(bw);
-                            }
-                            else if (section is Topology)
-                            {
-                                (section as Topology).StreamWrite(bw);
-                            }
-                            else if (section is PassthroughGP)
-                            {
-                                (section as PassthroughGP).StreamWrite(bw);
-                            }
-                            else if (section is TopologyIP)
-                            {
-                                (section as TopologyIP).StreamWrite(bw);
-                            }
-                            else if (section is Bones)
-                            {
-                                (section as Bones).StreamWrite(bw);
-                            }
-                            else if (section is SkinBones)
-                            {
-                                (section as SkinBones).StreamWrite(bw);
-                            }
-                            else if (section is QuatLinearRotationController)
-                            {
-                                (section as QuatLinearRotationController).StreamWrite(bw);
-                            }
-                            else if (section is LinearVector3Controller)
-                            {
-                                (section as LinearVector3Controller).StreamWrite(bw);
-                            }
-                            else
-                            {
-                                Log.Default.Warn("Tried to export an unknown section {0}, discarded.\n"
-                                    + "If there are subsequently a lot of problems with this model file,\n"
-                                    + "this was probably the problem. Please report this error immediately.",
-                                    section.GetType().FullName);
-                            }
-                        }
-
-                        if (data.leftover_data != null)
-                            bw.Write(data.leftover_data);
-
-
-                        fs.Position = 4;
-                        bw.Write((UInt32)fs.Length);
-
+                        anim_sec.StreamWrite(bw);
                     }
+
+                    foreach (Author author_sec in author_sections)
+                    {
+                        author_sec.StreamWrite(bw);
+                    }
+
+                    foreach (Material_Group mat_group_sec in material_group_sections)
+                    {
+                        mat_group_sec.StreamWrite(bw);
+                        foreach (uint id in mat_group_sec.items)
+                        {
+                            if (data.parsed_sections.Keys.Contains(id))
+                                (data.parsed_sections[id] as Material).StreamWrite(bw);
+                        }
+                    }
+
+                    foreach (Object3D obj3d_sec in object3D_sections)
+                    {
+                        obj3d_sec.StreamWrite(bw);
+                    }
+
+                    foreach (Model model_sec in model_sections)
+                    {
+                        model_sec.StreamWrite(bw);
+                    }
+
+
+                    foreach (SectionHeader sectionheader in data.sections)
+                    {
+                        if (!data.parsed_sections.Keys.Contains(sectionheader.id))
+                            continue;
+                        object section = data.parsed_sections[sectionheader.id];
+
+                        if (section is Unknown)
+                        {
+                            (section as Unknown).StreamWrite(bw);
+                        }
+                        else if (section is Animation ||
+                                section is Author ||
+                                section is Material_Group ||
+                                section is Material ||
+                                section is Object3D ||
+                                section is Model
+                            )
+                        {
+                            continue;
+                        }
+                        else if (section is Geometry)
+                        {
+                            (section as Geometry).StreamWrite(bw);
+                        }
+                        else if (section is Topology)
+                        {
+                            (section as Topology).StreamWrite(bw);
+                        }
+                        else if (section is PassthroughGP)
+                        {
+                            (section as PassthroughGP).StreamWrite(bw);
+                        }
+                        else if (section is TopologyIP)
+                        {
+                            (section as TopologyIP).StreamWrite(bw);
+                        }
+                        else if (section is Bones)
+                        {
+                            (section as Bones).StreamWrite(bw);
+                        }
+                        else if (section is SkinBones)
+                        {
+                            (section as SkinBones).StreamWrite(bw);
+                        }
+                        else if (section is QuatLinearRotationController)
+                        {
+                            (section as QuatLinearRotationController).StreamWrite(bw);
+                        }
+                        else if (section is LinearVector3Controller)
+                        {
+                            (section as LinearVector3Controller).StreamWrite(bw);
+                        }
+                        else
+                        {
+                            Log.Default.Warn("Tried to export an unknown section {0}, discarded.\n"
+                                + "If there are subsequently a lot of problems with this model file,\n"
+                                + "this was probably the problem. Please report this error immediately.",
+                                section.GetType().FullName);
+                        }
+                    }
+
+                    if (data.leftover_data != null)
+                        bw.Write(data.leftover_data);
+
+
+                    fs.Position = 4;
+                    bw.Write((UInt32)fs.Length);
+
                 }
-            }
-            catch (Exception exc)
-            {
-                System.Windows.Forms.MessageBox.Show(exc.ToString());
             }
         }
     }
