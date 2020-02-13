kythyria/payday2-model-tool

commit f7fab6c5cb5848de641f03549367f041f06919e9

Emshrinken models for GLTF export.

PD2ModelParser/Exporters/GltfExporter.cs
@@ -25,6 +25,15 @@ namespace PD2ModelParser.Exporters
         Dictionary<uint, List<(string, GLTF.Accessor)>> vertexAttributesByGeometryId;
         Dictionary<uint, GLTF.Material> materialsBySectionId;
 
+        /// <summary>
+        /// How much to embiggen data as it's converted to GLTF.
+        /// </summary>
+        /// <remarks>
+        /// See the remarks of <see cref="PD2ModelParser.Importers.GltfImporter.scaleFactor"/> 
+        /// for why the implementation here works.
+        /// </remarks>
+        float scaleFactor = 0.01f;
+
         GLTF.ModelRoot Convert(FullModelData data)
         {
             vertexAttributesByGeometryId = new Dictionary<uint, List<(string, GLTF.Accessor)>>();
@@ -53,7 +62,18 @@ namespace PD2ModelParser.Exporters
             var node = parent.CreateNode(thing.Name);
             if (thing != null)
             {
-                node.LocalMatrix = thing.rotation.ToMatrix4x4();
+                var istrs = thing.rotation.Decompose(out var scale, out var rotation, out var translation);
+                if(!istrs)
+                {
+                    throw new Exception($"In object \"{thing.Name}\" ({thing.SectionId}), non-TRS matrix");
+                }
+
+                var lt = SharpGLTF.Transforms.AffineTransform.Create(
+                    scale.ToVector3(),
+                    rotation.ToQuaternion(),
+                    translation.ToVector3() * scaleFactor
+                );
+                
             }
             if (thing is Model)
             {
@@ -135,7 +155,7 @@ namespace PD2ModelParser.Exporters
             }
             result = new List<(string, GLTF.Accessor)>();
 
-            var a_pos = MakeVertexAttributeAccessor("vpos", geometry.verts, 12, GLTF.DimensionType.VEC3, MathUtil.ToVector3, ma => ma.AsVector3Array());
+            var a_pos = MakeVertexAttributeAccessor("vpos", geometry.verts.Select(i=>i*scaleFactor).ToList(), 12, GLTF.DimensionType.VEC3, MathUtil.ToVector3, ma => ma.AsVector3Array());
             result.Add(("POSITION", a_pos));
 
             if (geometry.normals.Count > 0)

commit d630f6c72d931aec3dbe9de33fb98fa31090e9c8

Output transforms to GLTF as we should.
Fixes a regression introduced in f7fab6c.

PD2ModelParser/Exporters/GltfExporter.cs
@@ -73,6 +73,7 @@ namespace PD2ModelParser.Exporters
                     rotation.ToQuaternion(),
                     translation.ToVector3() * scaleFactor
                 );
+                node.LocalTransform = lt;
                 
             }
             if (thing is Model)