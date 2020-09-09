3422e6d44496c4c3d94e14282725227b74b3431b
Kythyria Tieran <kythyria@berigora.net>
2020-09-09 03:54:55 +0100

    Lay out the export panel according to control sizes.

--- PD2ModelParser/UI/ExportPanel.cs
+++ PD2ModelParser/UI/ExportPanel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using System.Windows.Forms.Layout;
 using PD2ModelParser.Modelscript;
 
 namespace PD2ModelParser.UI
@@ -10,6 +12,8 @@ namespace PD2ModelParser.UI
     public partial class ExportPanel : UserControl
     {
         private FullModelData model;
+        private ExportLayoutEngine layout = new ExportLayoutEngine();
+        public override LayoutEngine LayoutEngine => layout;
 
         public ExportPanel()
         {
@@ -60,6 +64,62 @@ namespace PD2ModelParser.UI
             Script.ExecuteItems(script, System.IO.Directory.GetCurrentDirectory());
 
             MessageBox.Show($"Successfully exported model {inputFileBox.Selected.Split('\\').Last()} (placed in the input model folder)");
+        }
+
+        class ExportLayoutEngine : LayoutEngine
+        {
+
+            public override bool Layout(object sender, LayoutEventArgs e)
+            {
+                var panel = (ExportPanel)sender;
+
+                var labels = new Label[] {
+                    panel.label1,
+                    panel.label2
+                };
+
+                var fields = new Control[]
+                {
+                panel.inputFileBox,
+                panel.formatBox
+                };
+
+                var maxLabelWidth = labels
+                    .Where(i => i != null)
+                    .Select(i => i.PreferredSize.Width + i.Margin.Horizontal).Max();
+                var currY = 0;
+
+                for (var i = 0; i < fields.Length; i++)
+                {
+                    var label = labels[i];
+                    var labelSize = label?.GetPreferredSize(new Size(1, 1)) ?? new Size(0, 0);
+                    var field = fields[i];
+                    var fieldSize = field.GetPreferredSize(new Size(1, 1));
+
+                    var rowHeight = Math.Max(labelSize.Height + (label?.Margin.Vertical ?? 0), fieldSize.Height + field.Margin.Vertical);
+
+                    if (label != null)
+                    {
+                        var labelOffsY = (rowHeight - labelSize.Height) / 2;
+                        label.SetBounds(maxLabelWidth - (labelSize.Width + label.Margin.Right), currY + labelOffsY, labelSize.Width, labelSize.Height);
+                    }
+
+                    var fieldX = maxLabelWidth + field.Margin.Left;
+                    var fieldOffsY = currY + (rowHeight - fieldSize.Height) / 2;
+                    var fieldWidth = panel.Width - (maxLabelWidth + field.Margin.Horizontal);
+                    field.SetBounds(fieldX, fieldOffsY, fieldWidth, fieldSize.Height);
+                    currY += rowHeight;
+                }
+
+                var buttonSize = panel.exportBttn.GetPreferredSize(new Size(1, 1));
+                panel.exportBttn.SetBounds(panel.exportBttn.Margin.Left, currY + panel.exportBttn.Margin.Top, panel.Width - panel.exportBttn.Margin.Horizontal, buttonSize.Height);
+                currY += panel.exportBttn.Bounds.Bottom + panel.exportBttn.Margin.Bottom;
+
+                //Console.WriteLine("End Layout");
+
+                panel.MinimumSize = new Size(panel.MinimumSize.Width, currY);
+                return true;
+            }
         }
     }
 }
